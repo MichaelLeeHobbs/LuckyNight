cb9c71ac5ab654287f196aef3969349ac279f51e
Michael Hobbs <michael.lee.hobbs@gmail.com>
2015-11-07 09:17:24 -0500

    Initial commit

---
+++ client/components/oauth-buttons/oauth-buttons.controller.js
@@ -0,0 +1,8 @@
+'use strict';
+
+angular.module('luckyNightApp')
+  .controller('OauthButtonsCtrl', function($window) {
+    this.loginOauth = function(provider) {
+      $window.location.href = '/auth/' + provider;
+    };
+  });
